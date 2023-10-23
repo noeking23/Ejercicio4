41b7c23f55a0ca86f5c02b700f1385a36e9100c5
Noe Magne <Noeca@HARLE_KING.com>
2023-10-23 09:36:11 -0400

    ejercicio4

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la cantidad de números a comparar:");
+        int n = int.Parse(Console.ReadLine());
+
+        int mayor = int.MinValue; // Inicializamos la variable mayor con el valor mínimo
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"Ingrese el número {i}: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > mayor)
+            {
+                mayor = numero; // Actualizamos el valor del mayor si encontramos un número más grande
+            }
+        }
+
+        Console.WriteLine($"El número mayor es: {mayor}");
+    }
+}
